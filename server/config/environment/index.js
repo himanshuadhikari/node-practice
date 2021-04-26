10257fd78f00b766990986f73e5603665a5b52ec
himanshu adhikari <himanshu.adhikari@tothenew.com>
2021-04-26 13:29:37 +0530

    hello world application created

---
+++ server/config/environment/index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var path = require("path");
+var environment = process.env.NODE_ENV || "development";
+
+function requiredProcessEnv(name) {
+  if (!process.env[name]) {
+    throw new Error("You must set the " + name + " environment variable");
+  }
+  return process.env[name];
+}
+
+// All configurations will extend these options
+// ============================================
+var all = {
+  env: process.env.NODE_ENV,
+
+  // Root path of server
+  root: path.normalize(__dirname + "/../../.."),
+
+  // Server port
+  port: process.env.PORT || 9000,
+
+  // Should we populate the DB with sample data?
+  seedDB: true,
+
+  // Secret for session, you will want to change this and make it an environment variable
+
+  // List of user roles
+  userRoles: ["GUEST", "USER", "ADMIN"],
+
+  // MongoDB connection options
+  mongo: {
+    options: {
+      db: {
+        safe: true,
+      },
+    },
+  },
+};
+
+// Export the config object based on the NODE_ENV
+// ==============================================
+module.exports = Object.assign(all, require("./" + environment + ".js") || {});
--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
 const cluster = require("cluster");
 const http = require("http");
 const numCPUs = require("os").cpus().length;
+const express = require("express");
+const port = 8000;
+const routes = require("./routes.js");
 
 if (cluster.isMaster) {
+  createChildProcess();
+} else {
+  const app = express();
+  createServer(app);
+}
+
+function createServer(app) {
+  routes(app);
+  http.createServer(app).listen(8000, function () {
+    console.info(
+      "APP: Server cluster :%d listening on %d, in %s mode",
+      cluster.isWorker ? cluster.worker.id : 1,
+      port,
+      app.get("env")
+    );
+  });
+}
+
+function createChildProcess() {
   console.log(`Master ${process.pid} is running`);
 
   // Fork workers.
@@ -16,42 +38,12 @@ if (cluster.isMaster) {
     );
   });
 
-  // cluster.on("listening", (worker, address) => {
-  //   console.log(
-  //     `listening == A worker is now connected to ${address.address}:${address.port}`
-  //   );
-  // });
-
-  // cluster.on("online", (worker) => {
-  //   console.log("online == Yay, the worker responded after it was forked");
-  // });
-
   cluster.on("disconnect", (worker) => {
     console.log(`The worker #${worker.process.pid} has disconnected`);
   });
 
   cluster.on("exit", (worker, code, signal) => {
-    console.log(
-      "worker %d died (%s). restarting...",
-      worker.process.pid,
-      signal || code
-    );
+    console.log("worker %d died (%s)", worker.process.pid, signal || code);
     // cluster.fork();
   });
-} else {
-  // Workers can share any TCP connection. In this case, it is an HTTP server.
-  console.log("child process executed");
-  http
-    .createServer((req, res) => {
-      console.log(
-        "===========================request came to server==========================="
-      );
-      res.writeHead(200);
-      res.end(`Current process\n ${process.pid}`);
-      console.log(
-        "===========================request finished from server==========================="
-      );
-      // process.kill(process.pid);
-    })
-    .listen(8000);
 }

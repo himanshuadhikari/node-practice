307efef78fc99682fb3007d24cdfda63679e8245
himanshu adhikari <himanshu.adhikari@tothenew.com>
2021-04-26 17:06:24 +0530

    created logger file

--- server/logger.js
+++ server/logger.js
@@ -1,7 +1,6 @@
 module.exports = function (app, cluster) {
   app.use((req, res, next) => {
-    console.log(`request came for url = ${req.url}`);
-    console.log(`request is processed by = ${cluster.worker.id}`);
+    console.log(`Current process ${process.pid}`);
     next();
   });
 };
